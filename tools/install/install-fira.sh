f095cd25abb1be36f8b26cfab281286e3eff91bb
Kevin Wilson local account <kevineato@users.noreply.github.com>
2020-05-15 11:21:30 -0500

    Add check for new font in order to skip 'fc-cache -f' when unnecessary.

--- tools/install/install-fira.sh
+++ tools/install/install-fira.sh
@@ -11,13 +11,17 @@ fi
 for type in Bold Light Medium Regular Retina; do
   file_path="${HOME}/.local/share/fonts/FiraCode-Nerd-Font-${type}.ttf"
   file_url="https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/${type}/complete/Fira%20Code%20${type}%20Nerd%20Font%20Complete.ttf?raw=true"
+  has_new_font=false
   if [ ! -e "${file_path}" ]; then
     echo "wget -O $file_path $file_url"
     wget -O "${file_path}" "${file_url}"
+    has_new_font=true
   else
     echo "Found existing file $file_path"
   fi;
 done
 
-echo "fc-cache -f"
-fc-cache -f
+if [ "$has_new_font" = true ]; then
+  echo "fc-cache -f"
+  fc-cache -f
+fi
