b807f3b51c7e5935622e8924d89c6b195553fcb8
Kevin Wilson local account <kevineato@users.noreply.github.com>
2020-06-19 17:36:33 -0500

    Vim QoL changes. Remove remainder of fzf.

--- zsh/export.zsh
+++ zsh/export.zsh
@@ -36,12 +36,12 @@ path_prepend "$HOME/.local/bin"
 export BAT_THEME='Gruvbox-N'
 
 # fzf
-export DISABLE_FZF_AUTO_COMPLETION='false'
-export DISABLE_FZF_KEY_BINDINGS='false'
-export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
-export FZF_DEFAULT_COMMAND="fd --type f --hidden --follow --ignore-file '~/.fdignore'"
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_DEFAULT_OPTS="--preview 'bat --style=numbers --color=always' --history=$HOME/.fzf_history --bind 'alt-a:toggle-all'"
+# export DISABLE_FZF_AUTO_COMPLETION='false'
+# export DISABLE_FZF_KEY_BINDINGS='false'
+# export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
+# export FZF_DEFAULT_COMMAND="fd --type f --hidden --follow --ignore-file '~/.fdignore'"
+# export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+# export FZF_DEFAULT_OPTS="--preview 'bat --style=numbers --color=always' --history=$HOME/.fzf_history --bind 'alt-a:toggle-all'"
 
 # General
 export EDITOR='nvim'
@@ -61,5 +61,5 @@ export GTAGSLABEL=pygments
 export RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
 
 # skim
-export SKIM_DEFAULT_COMMAND="fd --type f --hidden --follow --ignore-file '~/.fdignore'"
+export SKIM_DEFAULT_COMMAND="fd --type f --hidden --follow"
 export SKIM_DEFAULT_OPTIONS="--multi --preview-window=':hidden' --preview='bat --style=numbers --color=always {}' --cmd-history=$HOME/.skim_cmd_history --history=$HOME/.skim_history --bind='alt-a:toggle-all,alt-h:toggle-preview'"
