b29f4c9b47034f68b41c91a2188d928988fe61e2
kevineato <kevineato@users.noreply.github.com>
2022-02-07 15:02:05 -0600

    Only use neovim now.

--- tools/install/install-vim-plug.sh
+++ tools/install/install-vim-plug.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-NVIM_PLUG="${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim"
-VIM_PLUG="${HOME}/.dotfiles/vim/autoload/plug.vim"
+NVIM_PLUG="${HOME}/.dotfiles/config/nvim/site/autoload/plug.vim"
 
 if [ ! -f "${NVIM_PLUG}" ]; then
-  curl -fLo "${NVIM_PLUG}" --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
-
-if [ ! -f "${VIM_PLUG}" ]; then
-  mkdir -p "$(dirname ${VIM_PLUG})"
-  cp "${NVIM_PLUG}" "${VIM_PLUG}"
+    curl -fLo "${NVIM_PLUG}" --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
