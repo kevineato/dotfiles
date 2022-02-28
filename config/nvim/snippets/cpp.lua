8429e6324101f02d237538c0b9a75df45f41a0c1
kevineato <kevineato@users.noreply.github.com>
2022-02-28 15:51:08 -0600

    Conform to LuaSnip usage from CosmicNvim.

--- config/nvim/snippets/cpp.lua
+++ config/nvim/snippets/cpp.lua
@@ -1,8 +1,25 @@
 local ls = require("luasnip")
 local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
 local t = ls.text_node
 local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local events = require("luasnip.util.events")
+local ai = require("luasnip.nodes.absolute_indexer")
 
-local snippets = {}
+local M = {}
 
-return snippets
+M.snippets = {}
+
+M.map_snippets = function()
+	ls.snippets.c = ls.snippets.cpp
+	ls.snippets.h = ls.snippets.cpp
+	ls.snippets.hpp = ls.snippets.cpp
+	ls.snippets.tpp = ls.snippets.cpp
+end
+
+return M
