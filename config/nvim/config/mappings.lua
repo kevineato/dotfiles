6a1c2c1717800cb1264126e2abd61b9199078ab6
kevineato <kevineato@users.noreply.github.com>
2022-07-27 18:33:36 -0500

    feat: add useful substitution mapping

--- config/nvim/config/mappings.lua
+++ config/nvim/config/mappings.lua
@@ -36,3 +36,103 @@ map(
     "<Leader>x",
     "<Cmd>lua require('cosmic.config.utils').buf_kill('bd', 0, true)<CR>"
 )
+
+local replace = function()
+    local Input = require("nui.input")
+    local event = require("nui.utils.autocmd").event
+
+    local prompt_default = string.gsub(vim.fn.getreg("/"), [[\V]], "")
+    prompt_default = string.gsub(prompt_default, [[\<]], "")
+    prompt_default = string.gsub(prompt_default, [[\>]], "")
+    local popup_opts = {
+        position = {
+            row = 1,
+            col = 0,
+        },
+        size = {
+            width = #prompt_default > 25 and #prompt_default or 25,
+            height = 2,
+        },
+        relative = "cursor",
+        border = {
+            highlight = "FloatBorder",
+            style = "rounded",
+            text = {
+                top = "[Replace]",
+                top_align = "left",
+            },
+        },
+    }
+    local input_opts = {
+        prompt = "> ",
+        default_value = prompt_default,
+        on_submit = function(input)
+            if input then
+                vim.cmd([[
+                    function! Substitute()
+                        if getregtype("s") != ""
+                            let l:register = getreg("s")
+                        endif
+                        normal! qs
+                        redir => l:replacements
+                        try
+                            execute ",$s//]] .. input .. [[/gce#"
+                        catch /^Vim:Interrupt$/
+                            return
+                        finally
+                            normal! q
+                            let l:transcript = getreg("s")
+                            if exists("l:register")
+                                call setreg("s", l:register)
+                            endif
+                        endtry
+                        redir END
+
+                        if len(l:replacements) > 0
+                            let l:last = strpart(l:transcript, len(l:transcript) - 1)
+                            if l:last ==# "l" || l:last ==# "q" || l:last ==# "\<Esc>"
+                                return
+                            elseif l:last ==# "a"
+                                1,''-&ge
+                                return
+                            endif
+                        endif
+
+                        if line("''") > 1
+                            1,''-&&"
+                        endif
+                    endfunction
+                    call Substitute()
+                    delfunction! Substitute
+                ]])
+            end
+        end,
+    }
+
+    local input = Input(popup_opts, input_opts)
+
+    input:mount()
+
+    input:map("i", { "<Esc>", "<C-c>" }, function()
+        input.input_props.on_close()
+    end, { noremap = true }, false)
+
+    input:on(event.BufLeave, function()
+        input:unmount()
+    end)
+end
+
+vim.keymap.set("n", "<Leader>su", function()
+    vim.schedule(function()
+        vim.fn.setreg("/", vim.fn.expand("<cword>"))
+        replace()
+    end)
+end)
+
+vim.keymap.set("x", "<Leader>su", function()
+    vim.fn.feedkeys("y", "")
+    vim.schedule(function()
+        vim.fn.setreg("/", vim.fn.getreg([["]]))
+        replace()
+    end)
+end)
